4b41e5cc533b3392274566b39c5cbb20874f688b
Vinod Krishnamurthy <vinodkrishnamurthy@Vinods-MacBook-Pro-2.local>
2018-03-26 16:16:14 +1100

    completed Moves function and structural background design

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #This is our main file
+import copy
+
 
 def main(textfile):
     f = open(textfile, 'r')
@@ -7,86 +9,166 @@ def main(textfile):
     playingBoard = Board()
 
     #filling the board from textfile
-    for i in range(1,9):
-        for j in range(1,9):
+    for i in range(0,8):
+        for j in range(0,8):
             temp = f.read(1)
+            if temp == " " or temp == '\n':
+                temp = f.read(1)
+
             if temp == "-":
-                playingBoard[i][j] = Unnoccupied()
+                playingBoard.squares[i][j] = Unoccupied()
             elif temp == 'O':
-                playingBoard[i][j] = WhitePiece()
+                playingBoard.squares[i][j] = WhitePiece()
             elif temp == '@':
-                playingBoard[i][j] = BlackPiece()
-            else
-                playingBoard[i][j] = Corner()
+                playingBoard.squares[i][j] = BlackPiece()
+            elif temp == 'X':
+                playingBoard.squares[i][j] = Corner()
 
     templine = f.readline()
+    if templine == '\n':
+        templine = f.readline()
+    # print ("test2")
+    if templine == 'Moves\n':
+        # print ("test")
+        playingBoard.find_moves(True, playingBoard.w_moves)
+        print(str(len(playingBoard.w_moves)))
 
-    if templine == 'Moves':
-        return moves(playingBoard)
-    elif templine == 'Massacre':
-        return massacre(playingBoard)
+        playingBoard.find_moves(False, playingBoard.b_moves)
+        print(str(len(playingBoard.b_moves)))
+
+    # elif templine == 'Massacre':
+    #     return massacre(playingBoard)
 
     f.close()
 
-
-"""
-This function calculates the number of possible moves for White pieces and
-Black pieces respectively, printing them as two numbers on separate lines.
-
-"""
-def moves(tempBoard):
+    # for i in range(0,8):
+    #     for j in range(0,8):
+    #         print(playingBoard.squares[i][j].getName())
+    #     print ("\n")
 
 
-"""
-This function calculate a sequence of legal moves for White pieces that, if
-carried out starting from the given board configuration, would lead to all
-Black pieces being eliminated. This sequence of moves is printed as a series
-of coordinate pairs.
 
-"""
-def massacre(tempBoard):
+# This function calculates the number of possible moves for White pieces and
+# Black pieces respectively, printing them as two numbers on separate lines.
+
+# def moves(tempBoard):
+
+# asdasd
+
+
+# This function calculate a sequence of legal moves for White pieces that, if
+# carried out starting from the given board configuration, would lead to all
+# Black pieces being eliminated. This sequence of moves is printed as a series
+# of coordinate pairs.
+
+
+# def massacre(tempBoard):
+# '(' + str(a) + ', ' + str(b) + ') -> (' \
+# + str(x) + ', ' + str(y) + ')'
 
 
 class Board(object):
     def __init__(self):
-        self.squares = [Piece()*10 for i in range(10)]
-        for j in range(10): #creating buffer on top and bottom
-            self.squares[0][j] = OutOfBounds()
-            self.squares[9][j] = OutOfBounds()
-        for i in range(1,9): #creating buffer on left and right
-            self.squares[i][0] = OutOfBounds()
-            self.squares[i][9] = OutOfBounds()
+        self.squares = [[Piece() for i in range(8)] for j in range(8)]
+        # for j in range(8): #creating buffer on top and bottom
+        #     self.squares[0][j] = OutOfBounds()
+        #     self.squares[7][j] = OutOfBounds()
+        # for i in range(0,8): #creating buffer on left and right
+        #     self.squares[i][0] = OutOfBounds()
+        #     self.squares[i][9] = OutOfBounds()
         self.whiteToMove = True
+        # self.num_moves = self.moves()
+        # ADD COUNTING OF MOVES ^
+        self.w_moves = {}
+        self.b_moves = {}
+
+
+    # a,b refers to the original coordinates
+    # x,y refers to the new coordinates
+    def generate_move(self, a, b, x, y):
+        tempKey = str(a) + str(b) + str(x) + str(y)
+        tempBoard = self
+        tempBoard.whiteToMove = not self.whiteToMove
+        # tempBoard.squares[x][y] = tempBoard.squares[a][b]
+        # tempBoard.squares[a][b] = Unoccupied()
+        return {tempKey: tempBoard}
+
+    def find_moves(self, isWhite, tempDict):
+        pieceColor = "BlackPiece"
+        if isWhite:
+            pieceColor = "WhitePiece"
+        for i in range(0,8):
+            for j in range(0,8):
+                if self.squares[i][j].getName() == pieceColor:
+                    # check left possible moves
+                    if (i - 1 >= 0):
+                        if (self.squares[i-1][j].getName() == "Unoccupied"):
+                            tempDict.update(self.generate_move(i, j, i-1, j))
+                        elif (i - 2 >= 0):
+                            if (self.squares[i-2][j].getName() == "Unoccupied"):
+                                tempDict.update(self.generate_move(i, j, i-2, j))
+
+                    # check right possible moves
+                    if (i + 1 <= 7):
+                        if (self.squares[i+1][j].getName() == "Unoccupied"):
+                            tempDict.update(self.generate_move(i, j, i+1, j))
+                        elif (i + 2 <= 7):
+                            if (self.squares[i+2][j].getName() == "Unoccupied"):
+                                tempDict.update(self.generate_move(i, j, i+2, j))
+
+                    # check above possible moves
+                    if (j - 1 >= 0):
+                        if (self.squares[i][j-1].getName() == "Unoccupied"):
+                            tempDict.update(self.generate_move(i, j, i, j-1))
+                        elif (j - 2 >= 0):
+                            if (self.squares[i][j-2].getName() == "Unoccupied"):
+                                tempDict.update(self.generate_move(i, j, i, j-2))
+
+                    # check bottom possible moves
+                    if (j + 1 <= 7):
+                        if (self.squares[i][j+1].getName() == "Unoccupied"):
+                            tempDict.update(self.generate_move(i, j, i, j+1))
+                        elif (j + 2 <= 7):
+                            if (self.squares[i][j+2].getName() == "Unoccupied"):
+                                tempDict.update(self.generate_move(i, j, i, j+2))
+
+
+
+
+
 
 
 class Piece(object):
     def __init__(self):
-
-    def getName(): #gets name of piece, will be customized when inherited
+        self.name = ""
+    def getName(self): #gets name of piece, will be customized when inherited
         return "Piece"
 
 
 class WhitePiece(Piece):
-    def getName():
+    def getName(self):
         return "WhitePiece"
 
 
 
 class BlackPiece(Piece):
-    def getName():
+    def getName(self):
         return "BlackPiece"
 
 
 class Unoccupied(Piece):
-    def getName():
+    def getName(self):
         return "Unoccupied"
 
 
 class Corner(Piece):
-    def getName():
+    def getName(self):
         return "Corner"
 
 
 class OutOfBounds(Piece):
-    def getName():
+    def getName(self):
         return "OOB"
+
+
+main("sample_files/move-sample-3.in")
