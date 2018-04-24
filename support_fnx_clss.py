6b3d4d031a855b295068d0c9c70bf7f3a149f71d
Rowan McEvoy <Rowan@MacBook-Pro-2.local>
2018-04-24 16:18:46 +1000

    Started to create heuristic function with arbitrary weights

--- support_fnx_clss.py
+++ support_fnx_clss.py
@@ -78,3 +78,120 @@ def print_board(tempBoard):
             tempString = tempString + tempBoard[col][row] + ' '
         tempString = tempString + "\n"
     print(tempString)
+
+class QueueItem
+    def __init__(self, our_token, depth = 0, runningMoves = []):
+        self.depth = depth
+        self.runningMoves = runningMoves
+        self.heuristic = 0
+        self.token = our_token
+
+        self.opp_token = 'O'
+        if (self.token == 'O'):
+            self.opp_token = '@'
+
+    # execute move, calculate heuristic, undo move
+    def calc_h(self, board):
+
+        execute_moves(board, self.runningMoves)
+
+        self.heuristic = 20*self.own_pieces(board) - 18*self.opp_pieces(board) \
+        - 4*self.our_corners(board) + 4*self.opp_corners(board) \
+        + 7*self.surr_area_comp(board)
+
+        undo_moves(board, self.runningMoves)
+    def our_pieces(self, board):
+        counter = 0
+        for i in range(7-edge, edge+1):
+            for j in range(7-edge, edge+1):
+                if (board[i][j] == self.token):
+                    counter = counter + 1
+        return counter
+
+    def opp_pieces(self, board):
+        counter = 0
+        for i in range(7-edge, edge+1):
+            for j in range(7-edge, edge+1):
+                if (board[i][j] == self.opp_token):
+                    counter = counter + 1
+        return counter
+
+    #number of own pieces adjacent to corners
+    def our_corners(self, board):
+        counter = 0
+        #top left
+        if (board[7-self.edge+1][7-self.edge] == self.token):
+            counter = counter + 1
+        if (board[7-self.edge][7-self.edge+1] == self.token):
+            counter = counter + 1
+        #bottom left
+        if (board[7-self.edge+1][self.edge] == self.token):
+            counter = counter + 1
+        if (board[7-self.edge][self.edge-1] == self.token):
+            counter = counter + 1
+        #bottom right
+        if (board[self.edge-1][self.edge] == self.token):
+            counter = counter + 1
+        if (board[self.edge][self.edge-1] == self.token):
+            counter = counter + 1
+        #top right
+        if (board[self.edge-1][7-self.edge] == self.token):
+            counter = counter + 1
+        if (board[self.edge][7-self.edge+1] == self.token):
+            counter = counter + 1
+
+        return counter
+
+    #number of opponent pieces adjacent to corners
+    def opp_corners(self, board):
+        counter = 0
+        #top left
+        if (board[7-self.edge+1][7-self.edge] == self.opp_token):
+            counter = counter + 1
+        if (board[7-self.edge][7-self.edge+1] == self.opp_token):
+            counter = counter + 1
+        #bottom left
+        if (board[7-self.edge+1][self.edge] == self.opp_token):
+            counter = counter + 1
+        if (board[7-self.edge][self.edge-1] == self.opp_token):
+            counter = counter + 1
+        #bottom right
+        if (board[self.edge-1][self.edge] == self.opp_token):
+            counter = counter + 1
+        if (board[self.edge][self.edge-1] == self.opp_token):
+            counter = counter + 1
+        #top right
+        if (board[self.edge-1][7-self.edge] == self.opp_token):
+            counter = counter + 1
+        if (board[self.edge][7-self.edge+1] == self.opp_token):
+            counter = counter + 1
+
+        return counter
+
+    # surrounding area composition for all own pieces
+    def surr_area_comp(self, board):
+        counter = 0
+        for i in range(7-edge, edge+1):
+            for j in range(7-edge, edge+1):
+                if (board[i][j] == self.token):
+                    for dx, dy in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]:
+                        if ((i+dx in range(7-edge, edge+1)) and (j+dy in range(7-edge, edge+1))):
+                            if (board[i+dx][j+dy] == self.token):
+                                counter = counter + 1
+                            elif (board[i+dx][j+dy] == self.opp_token):
+                                counter = counter - 1
+        return counter
+
+    def num_jumps(self, board):
+        pass
+
+# do minimax implementation here
+def choose_move(board, colour):
+    #create priority queue of moves
+    pass
+
+def execute_moves(board, runningMoves):
+    pass
+
+def undo_moves(board, runningMoves):
+    pass
